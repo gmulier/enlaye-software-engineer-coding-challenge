45e6da5975d27f5ccdf8ea32081175d8162e0cf6
konrad-enlaye <konrad@enlaye.com>
2025-12-04 16:37:32 -0500

    Show file tree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { FileTree } from "./FileTree";
 import { UploadFiles } from "./UploadFiles";
 
 export default function Home() {
   return (
     <div className="container mx-auto px-4 py-10">
       <h1 className="text-2xl font-bold mb-6">Enlaye Files</h1>
+      <FileTree />
       <UploadFiles />
     </div>
   );
